795694e318f61ca8ff6e1564672ba3e6544fb4fd
Battlecstasy <battlecstasy@gmail.com>
2017-05-10 12:37:58 -0300

    Add files via upload

---
+++ Cirno.lua
@@ -0,0 +1,17 @@
+local Cirno = {}
+local CirnoWalkSpeed = 1.1
+local CirnoWlakSpeedBack = 1.1
+local CirnoDashSpeed = 1.2
+local CirnoGravity = 0.8
+local CirnoJump = 1.1 
+local CirnoHP = 92
+local CirnoAnimationFrame
+function Cirno:new()
+
+end
+
+function Cirno:update()
+  
+end
+
+return Cirno
